d90cde8ef1f0c4196452c86b0a0ec48587c12c31
RybalkoA <rybalko8373@gmail.com>
2016-03-29 18:02:12 +0300

    Add vendor

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,132 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '2.0.3.0',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'skeeks/yii2-template-tshop' => 
+  array (
+    'name' => 'skeeks/yii2-template-tshop',
+    'version' => '1.5.0.0',
+    'alias' => 
+    array (
+      '@skeeks/template/tshop' => $vendorDir . '/skeeks/yii2-template-tshop',
+    ),
+  ),
+  'skeeks/yii2-template-smarty' => 
+  array (
+    'name' => 'skeeks/yii2-template-smarty',
+    'version' => '1.1.3.0',
+    'alias' => 
+    array (
+      '@skeeks/template/smarty' => $vendorDir . '/skeeks/yii2-template-smarty',
+    ),
+  ),
+  'rmrevin/yii2-fontawesome' => 
+  array (
+    'name' => 'rmrevin/yii2-fontawesome',
+    'version' => '2.13.0.0',
+    'alias' => 
+    array (
+      '@rmrevin/yii/fontawesome' => $vendorDir . '/rmrevin/yii2-fontawesome',
+    ),
+  ),
+  'cebe/yii2-gravatar' => 
+  array (
+    'name' => 'cebe/yii2-gravatar',
+    'version' => '1.1.0.0',
+    'alias' => 
+    array (
+      '@cebe/gravatar' => $vendorDir . '/cebe/yii2-gravatar/cebe/gravatar',
+    ),
+  ),
+  'dmstr/yii2-adminlte-asset' => 
+  array (
+    'name' => 'dmstr/yii2-adminlte-asset',
+    'version' => '2.2.4.0',
+    'alias' => 
+    array (
+      '@dmstr' => $vendorDir . '/dmstr/yii2-adminlte-asset',
+    ),
+  ),
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'yiisoft/yii2-codeception' => 
+  array (
+    'name' => 'yiisoft/yii2-codeception',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/codeception' => $vendorDir . '/yiisoft/yii2-codeception',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'costa-rico/yii2-images' => 
+  array (
+    'name' => 'costa-rico/yii2-images',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@rico/yii2images' => $vendorDir . '/costa-rico/yii2-images',
+    ),
+  ),
+  'andreykluev/yii2-shop-basket' => 
+  array (
+    'name' => 'andreykluev/yii2-shop-basket',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@andreykluev/shopbasket' => $vendorDir . '/andreykluev/yii2-shop-basket',
+    ),
+  ),
+  'omnilight/yii2-shopping-cart' => 
+  array (
+    'name' => 'omnilight/yii2-shopping-cart',
+    'version' => '1.2.2.0',
+    'alias' => 
+    array (
+      '@yz/shoppingcart' => $vendorDir . '/omnilight/yii2-shopping-cart',
+    ),
+  ),
+);
